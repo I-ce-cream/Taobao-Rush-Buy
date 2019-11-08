4cb2d4dc769e281910f0dfeaa5d6cee835941080
Zhangxize <541459574@qq.com>
2019-11-08 10:01:00 +0800

    初始化

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+import time
+import login
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+driver.get("http://www.baidu.com")
+
+
+def login():
+
+
+def buy(time,choose):
+
+
+
+
+
+if __name__ = "__main__" :
